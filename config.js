ac575bc12bfcffa657c31415b1c9b4bbb0b133ed
RAFAEL-QUESADA <rafaelquesadaal@gmail.com>
2018-07-25 10:56:44 -0600

    Revert "- NodeJs backend for Musify demo"
    This reverts commit 9d8c1a57207509ed83d6851521b6208cb17cd90c.

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    port: process.env.PORT || 3977,
-    db: process.env.MONGODB || 'mongodb://localhost:27017/mean-project',
-    userUploadDir: './uploads/users',
-    artistsUploadDir: './uploads/artists',
-    albumsUploadDir: './uploads/albums',
-    songsUploadDir: './uploads/songs',
-    SECRET_TOKEN: 'secret_password'
-};
